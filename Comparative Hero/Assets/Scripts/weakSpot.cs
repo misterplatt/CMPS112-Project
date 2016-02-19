455d4785a064d1df149d601e83ea77774d579ccf
unknown <hplatt@link.adgpm.soe.ucsc.edu>
2016-02-18 16:59:09 -0800

    Initial commit: Moving, jumping, enemies, dying

---
+++ Comparative Hero/Assets/Scripts/weakSpot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class weakSpot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player") Destroy(gameObject.transform.parent.gameObject);
+    }
+}
