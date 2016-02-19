ea8850eb63cfd688d948d7453779ef7d1fb46597
unknown <hplatt@link.adgpm.soe.ucsc.edu>
2016-02-18 17:10:19 -0800

    Renamed scripts to reflect language

---
+++ Comparative Hero/Assets/Scripts/Csharp_enemyController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Csharp_enemyController : MonoBehaviour {
+
+    public float speed = 3f;
+
+    private Vector3 initialPosition;
+    private bool left = false;
+
+	// Use this for initialization
+	void Start () {
+        initialPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x >= initialPosition.x + 3) left = true;
+        if (transform.position.x < initialPosition.x) left = false;
+
+        if (!left)transform.Translate(Vector3.right * Time.deltaTime * speed);
+        if (left)transform.Translate(Vector3.left * Time.deltaTime * speed);
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player") Destroy(col.gameObject);
+    }
+}
---
+++ Comparative Hero/Assets/Scripts/Csharp_playerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Csharp_playerController : MonoBehaviour {
+
+    public float speed = 5f;
+    public float jumpForce = 250f;
+
+    private bool grounded = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Left-Right Movement
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && grounded) {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+            grounded = false;
+        }
+    }
+
+    //Grounding
+    void OnCollisionEnter2D(Collision2D col) {
+        Debug.Log(col.gameObject.name);
+        if (col.gameObject.tag == "ground") grounded = true;
+        //if (col.gameObject.name == "weakSpot") Destroy(col.gameObject.transform.parent);
+    }
+}
